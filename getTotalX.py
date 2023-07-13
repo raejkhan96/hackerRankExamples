2915a740510194bc6e5e58592af6ec0499b6ff56
Rae J. Khan <raekhan18@gmail.com>
2023-07-13 12:42:41 -0400

    between 2 sets is a hard problem

--- getTotalX.py
+++ getTotalX.py
@@ -16,45 +16,70 @@ import sys
 #
 
 def getTotalX(a, b):
-    # Write your code here
-    factors, factors2 = [], []
-    # Go through the entire array b
-    for i in range(len(b)):
-        # for each number in array b
-        for j in range(2, b[i]):
-            # find the factors of each number
-            if ((b[i] % j) == 0):
-                factors.append(j)
 
-    #  first part needs to be reworked. as opposed to adding everything find the factors in common 
+    # OBTAINED SOLUTION:
+    # maximum of a is required because if you check the max - you dont need the rest
+    max_a = max(a)
+    # minium of b is required because we need all the values/factors to be in common - so we take the lowest value
+    min_b = min(b)
+    count = 0
+
+    # min_b +1 because we are up to and not including
+    for num in range(max_a, min_b + 1):
+        if all(num % factor == 0 for factor in a) and all(factor % num == 0 for factor in b):
+            count += 1
+
+    return count
+
+    # MY SOLUTION - DOES NOT WORK:
+    # # Write your code here
+    # factors, factors2 = [], []
+    # # Go through the entire array b
+    # for i in range(len(b)):
+    #     # for each number in array b
+    #     for j in range(2, b[i]):
+    #         # find the factors of each number
+    #         if ((b[i] % j) == 0):
+    #             factors.append(j)
+
+    # #  first part needs to be reworked. as opposed to adding everything find the factors in common 
     
-    print(factors)
-    factors_no_duplicates = set(factors)
-    print(factors_no_duplicates)
-    factors_no_duplicates = list(factors_no_duplicates)
-    print(factors_no_duplicates)
+    # print(factors)
+    # factors_no_duplicates = set(factors)
+    # print(factors_no_duplicates)
+    # factors_no_duplicates = list(factors_no_duplicates)
+    # print(factors_no_duplicates)
 
-    for c in range(len(a)):
-        print(a[c])
+    # # i, j = 0, 0
+    # for i in range(len(b)):
+    #     for j in range(len(factors_no_duplicates) - 1):
+    #         print(j)
+    #         print(factors_no_duplicates[j])
+    #         if (b[i] % factors_no_duplicates[j] != 0):
+    #             factors_no_duplicates.remove(factors_no_duplicates[j])
 
-    for k in range(len(factors_no_duplicates)):
-        count = 0
-        for c in range(len(a)):
-            if ((factors_no_duplicates[k] % a[c]) == 0):
-                count += 1
-        if (count == len(a)):
-            factors2.append(factors_no_duplicates[k])
+    # # print(factors_no_duplicates)
+    # # for c in range(len(a)):
+    # #     print(a[c])
 
-    print(factors2)
+    # for k in range(len(factors_no_duplicates)):
+    #     count = 0
+    #     for c in range(len(a)):
+    #         if ((factors_no_duplicates[k] % a[c]) == 0):
+    #             count += 1
+    #     if (count == len(a)):
+    #         factors2.append(factors_no_duplicates[k])
 
-    return 5
+    # print(factors2)
+
+    # return len(factors2)
 
 
        
     
-    # factors3 = set(factors2)
-    # print(factors3)
-    # return len(factors3)
+    # # factors3 = set(factors2)
+    # # print(factors3)
+    # # return len(factors3)
 
                        
 
