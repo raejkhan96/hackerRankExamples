77b56721fb7c28288ab70c8ed2bd0adafd291296
Rae J. Khan <raekhan18@gmail.com>
2023-07-28 22:29:41 -0400

    improvement on page count function, passed more test cases

--- pageCount.py
+++ pageCount.py
@@ -17,28 +17,33 @@ import sys
 
 def pageCount(n, p):
     # Write your code here
+    # MY SOLUTION:
     arr1, arr2 = [], []
     count1, count2 = 0, 0
+    arr1.append([1])
     # forwards
     for i in range(2, n, 2):
         arr1.append([i, i+1])
     # print(arr1)
     for i in range(len(arr1)):
-        count1 += 1
         if p in arr1[i]:
             break
+        else:
+            count1 += 1
     # backwards 
-    for j in range(n-1, 0, -2):
-        arr2.append([j, j-1])
+    arr2 = arr1[::-1]
     # print(arr2)
-    for j in range(len(arr2)):
-        count2 += 1
+    for j in range(0, len(arr2)):
+        print(arr2[j])
         if p in arr2[j]:
             break
-
+        else:
+            count2 += 1
+    # print("COUNT1 ", count1)
+    # print("COUNT2 ", count2)
     return min(count1, count2)
 
-n = 6
-p = 2
+n = 5
+p = 4
 result = pageCount(n, p)
 print(result)
