8b64cfeb35344c1b377c4a5f2ac221d769b12da0
Rae J. Khan <raekhan18@gmail.com>
2023-08-01 23:47:52 -0400

    file initilization and problem review

---
+++ getMoneySpent.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+
+
+moneySpent = getMoneySpent(keyboards, drives, b)
