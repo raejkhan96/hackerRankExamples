b340548612d17931b327b58e99f209bdb5110d1c
Rae J. Khan <raekhan18@gmail.com>
2023-08-04 20:59:43 -0400

    found actual solution

--- getMoneySpent.py
+++ getMoneySpent.py
@@ -7,23 +7,41 @@ import sys
 # Complete the getMoneySpent function below.
 #
 def getMoneySpent(keyboards, drives, b):
-    arr = []
-    # Write your code here.
-    sorted_keyboards = sorted(keyboards, reverse=True)
-    for i in range(len(sorted_keyboards)):
-        if (sorted_keyboards[i] < b):
-            start = i
-            break;
 
-    for j in range(start, len(sorted_keyboards)):
-        for k in range(len(drives)):
-            if ((sorted_keyboards[j] + drives[k]) < b):
-                arr.append(sorted_keyboards[j] + drives[k])
+    # actual solution 
+    keyboards.sort()
+    drives.sort()
+    # print(keyboards)
+    # print(drives)
+    res = -1
+    for k in keyboards:
+        x = -1
+        for d in drives:
+            if k + d <= b:
+                x = k + d
+        if x == -1:
+            break
+        else:
+            res = max(res, x)
+    return res
 
-    if not arr:
-        return('-1')
-    else:
-        return(max(arr))
+    # My solution - didnt pass all tests
+    # arr = []
+    # sorted_keyboards = sorted(keyboards, reverse=True)
+    # for i in range(len(sorted_keyboards)):
+    #     if (sorted_keyboards[i] < b):
+    #         start = i
+    #         break;
+
+    # for j in range(start, len(sorted_keyboards)):
+    #     for k in range(len(drives)):
+    #         if ((sorted_keyboards[j] + drives[k]) < b):
+    #             arr.append(sorted_keyboards[j] + drives[k])
+
+    # if not arr:
+    #     return('-1')
+    # else:
+    #     return(max(arr))
 
 b = 60
 keyboards = [40, 50, 60]
