663744b0cd1a4b90e4a6a06ca7947c09ef082faa
Rae J. Khan <raekhan18@gmail.com>
2023-08-06 22:38:53 -0400

    file initialization complete

---
+++ designerPdfViewer.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+
+
+
+result = designerPdfViewer(h, word)
