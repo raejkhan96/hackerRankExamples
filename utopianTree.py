05aff0b82aa7a780f8f99890a9357479653c0a6e
Rae J. Khan <raekhan18@gmail.com>
2023-08-13 20:15:02 -0400

    got basic algorithm

--- utopianTree.py
+++ utopianTree.py
@@ -15,7 +15,15 @@ import sys
 
 def utopianTree(n):
     # Write your code here
-
-
-
+    height = 1
+    for num in range(0, n):
+        print("PERIOD ", num)
+        if num % 2 == 0:
+            height = height * 2
+            print("HEIGHT: ", height)
+        elif (num != 0):
+            height = height + 1
+            print("HEIGHT: ", height)
+n = 5
 result = utopianTree(n)
+print(result)
