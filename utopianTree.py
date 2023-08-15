2af05b64ad76bfaa9dc15dfbeb24fb812586a3bf
Rae J. Khan <raekhan18@gmail.com>
2023-08-14 20:32:08 -0400

    tried and failed alternate solution

--- utopianTree.py
+++ utopianTree.py
@@ -16,14 +16,20 @@ import sys
 def utopianTree(n):
     # Write your code here
     height = 1
+    on = True
     for num in range(0, n):
         print("PERIOD ", num)
-        if num % 2 == 0:
-            height = height * 2
+        if num == 0:
             print("HEIGHT: ", height)
-        elif (num != 0):
-            height = height + 1
+            on = False
+        elif on == False:
+            height = abs(height + 1)
             print("HEIGHT: ", height)
+            on = False
+        else:
+            height = abs(height * 2)
+            print("HEIGHT: ", height)
+            on = True
 n = 5
 result = utopianTree(n)
 print(result)
