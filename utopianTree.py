09be361c72452599ce134b8b224453b93a3e1e1d
Rae J. Khan <raekhan18@gmail.com>
2023-08-16 21:36:28 -0400

    finally solved problem

--- utopianTree.py
+++ utopianTree.py
@@ -17,19 +17,25 @@ def utopianTree(n):
     # Write your code here
     height = 1
     on = True
-    for num in range(0, n):
+    for num in range(0, n+1):
         print("PERIOD ", num)
         if num == 0:
-            print("HEIGHT: ", height)
+            # print("HEIGHT: ", height)
+            # print(on)
             on = False
         elif on == False:
-            height = abs(height + 1)
-            print("HEIGHT: ", height)
-            on = False
-        else:
             height = abs(height * 2)
-            print("HEIGHT: ", height)
+            # print("HEIGHT: ", height)
+            # print(on)
             on = True
+        else:
+            height = abs(height + 1)
+            # print("HEIGHT: ", height)
+            # print(on)
+            on = False
+    
+    return height
+
 n = 5
 result = utopianTree(n)
 print(result)
