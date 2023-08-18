c548ca44c68ab538c4ba508e79f5cbcbbce6d825
Rae J. Khan <raekhan18@gmail.com>
2023-08-17 22:16:00 -0400

    file initialization

---
+++ angryProfessor.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a):
+    # Write your code here
+
+result = angryProfessor(k, a)
