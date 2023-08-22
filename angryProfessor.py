35fa8f67cc0bbbf24197b2119e545701fab42633
Rae J. Khan <raekhan18@gmail.com>
2023-08-21 20:31:51 -0400

    finally solved problem

--- angryProfessor.py
+++ angryProfessor.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import mathcd 
+import math 
 import os
 import random
 import re
@@ -19,10 +19,15 @@ def angryProfessor(k, a):
     # Write your code here
     # need to count the amount of values are before and at  0
     # if it matches k, return NO, else YES
+    count = 0
     for x in a:
-        if (x <= 0)
-   
+        if (x <= 0):
+            count +=1 
+    if (count >= k):
+        return 'NO'
+    return 'YES'
 
 k = 3
 a = [-2, -1, 0, 1, 2]
 result = angryProfessor(k, a)
+print(result)
