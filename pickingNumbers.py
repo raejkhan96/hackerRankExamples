f828aa374b312d547b6d8dcd3932adde53e5151b
Rae J. Khan <raekhan18@gmail.com>
2023-08-22 21:56:24 -0400

    file initialization

---
+++ pickingNumbers.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+
+
+
+
+result = pickingNumbers(a)
+print(a)
