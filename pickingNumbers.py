ebc288dac88e90cff8f42e22c212923fbab6c609
Rae J. Khan <raekhan18@gmail.com>
2023-08-24 22:11:04 -0400

    pseudocode solution added

--- pickingNumbers.py
+++ pickingNumbers.py
@@ -15,7 +15,13 @@ import sys
 
 def pickingNumbers(a):
     # Write your code here
-
+    # pseudo code - determine length of array, 
+    # find 5th element from behind as max length is 5 of sub array
+    # start from beginning or end
+    # loop through the array ensure that each difference is less than 1
+    # if the subarray length is less than 5 or at 5, stop
+    # continue the process with next subarray till determined marker - 5th row from end
+     
 
 
 
