4350de32e99b57c6cda871ce0ab45d4462413fc2
Rae J. Khan <raekhan18@gmail.com>
2023-08-28 21:31:25 -0400

    picking numbers attempt

--- pickingNumbers.py
+++ pickingNumbers.py
@@ -25,14 +25,18 @@ def pickingNumbers(a):
     # then find max length
 
     big_arr = []
+    new_arr = []
     for i in range(len(a) - 1):
-       current = i
-       next = a[i+1]
-       if (abs(next - current) >= 1):
-           
-    return 6 
+        current = i
+        next = a[i+1]
+        if (abs(next - current) > 1):
+           big_arr.append(new_arr)
+           new_arr = []
+        else:
+            new_arr.append(current)
+    return big_arr 
 
 
 a = [1,1,2,2,4,4,5,5,5]
 result = pickingNumbers(a)
-print(a)
+print(result)
